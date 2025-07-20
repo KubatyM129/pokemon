25ec6216af2cae7af056897c2e11d2616d1788e3
Mateusz Kubaty <115424949+KubatyM129@users.noreply.github.com>
2025-07-20 17:14:33 +0200

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader: React.FC = () => {
     return (
         <div className="flex flex-col items-center w-full">
-            <img className='max-w-32 max-h-32' src="\src\assets\pikachu.gif"/>
+            <img className='max-w-32 max-h-32' src="pikachu.gif"/>
             <p>Loading...</p>
         </div>
     );
